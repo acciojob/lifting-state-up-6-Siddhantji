55846e4828cdffddf27cf0aa95e2eed98b2b7a99
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-07-18 02:45:18 +0530

    Create List.js

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const List = ({ text }) => {
+  const [mark, setMark] = useState(false);
+  return (
+    <li>
+      {text}
+      {!mark ? <button onClick={() => setMark(true)}>Complete</button> : ""}
+    </li>
+  );
+};
+
+export default List;
