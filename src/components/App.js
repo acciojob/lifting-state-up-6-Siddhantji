71322226481d77a160d5e9d8876fb97418fc3435
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-07-18 02:44:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
-import './../styles/App.css';
+import Todo from "./Todo.js";
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <Todo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
